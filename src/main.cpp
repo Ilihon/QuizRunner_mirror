ec2dd4d6e142dffaac0078a7376f60a83f2050b6
ip913s02 <almich18@mail.ru>
2020-05-06 18:52:28 +0700

    Add question structure to main.cpp.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cctype>
+#include <string.h>
+
+using namespace std;
+
+struct quest{
+  string question;
+  string first_answer;
+  string second_answer;
+  string third_anwser;
+  string fourth_answer;
+  int right_index = 0;
+};
+struct quest test[10];
+
+
+int main(){
+
+  return 0;
+}
