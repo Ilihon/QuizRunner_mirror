6517ed3ab309895047c8b27e12635a713732872b
ip913s02 <almich18@mail.ru>
2020-05-07 22:03:30 +0700

    Cover loadfiles function.

---
+++ test-cover/test.cpp
@@ -0,0 +1,19 @@
+#include "../src/testload.h"
+#include "../src/testwork.h"
+
+struct quest test[10];
+int right_answers[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int score = 0;
+
+TEST(LoadfilesTest, MissingFile)
+{
+  int result = loadfiles("files/MissingFile.txt");
+  int expect = -1;
+  EXPECT_EQ(expected, result);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
